c65620eedfd3ee671a9804ae33df1aca3adfa804
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-04-11 13:29:29 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,16 +6,38 @@
 -- This file will check if you have t ogfo to chsol.
 -----------------------------------------------------------------------------------------
 
-day = ""
+local background = display.setDefault( "background", 255, 0, 0)
 
-age = 0
+local ageBox = native.newTextField( 160, 150, 200, 40)
 
-local ageBox = native.newTextField( 160, 200, 200, 40)
+local dayBox = native.newTextField(160, 200, 200, 40)
 
-local 
+local button = display.newRect(160, 300, 100, 100)
+
+local output = display.newText( "", 160, 400, "Arial", 25)
+
+local weirdText = display.newText( "", 160, 460, 200, 100, "Arial", 23)
 
 local function Check ( event )
 
-	if ((age < 18) and ( day ~= "Saturday" or day ~= "Sunday")) then
-		print("go to school")
+local age = tonumber(ageBox.text)
+
+weirdness(age) 
+
+	if ((age < 18) and (day ~= "Saturday" or day ~= "sunday" or day ~= "Sunday" or day ~= "sunday")) then
+		output.text = "Time for School!"
+	elseif ((age > 18) and (day ~= "Saturday" or day ~= "sunday" or day ~= "Sunday" or day ~= "sunday")) then
+		output.text = "Time for Work!"
+	else
+		output.text = "Time to relax for the weekend!"
 	end
+end
+
+button:addEventListener( "touch", Check)
+
+function weirdness(age)
+
+	if (age > 120) then
+		weirdText.text = "That's suspicious, you're awfully old!"
+	end
+end
